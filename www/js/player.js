d9aa96c87fc75ef8fcc515a032d1783a4e58f4ed
boukeversteegh <boukeversteegh@gmail.com>
2013-01-17 12:51:33 +0100

    restructuring. WIP

---
+++ www/js/player.js
@@ -0,0 +1,20 @@
+function Player() {
+	this.jplayer = null;
+	this.jplayerid = null;
+	
+	this.init = function(jplayerid) {
+		this.jplayerid = jplayerid;
+	}
+	
+	this.jp = function() {
+		if( this.jplayer == null ) {
+			this.jplayer = $(this.jplayerid);
+		}
+		return this.jplayer;
+	}
+	
+	this.playMedia = function(url, data) {
+		//this.jp().jPlayer("stop");
+		this.jp().jPlayer("setMedia", {mp3: url}).jPlayer('play');
+	}
+}
---
+++ www/js/ui.js
@@ -0,0 +1,27 @@
+$(function() {
+
+	// PLAYER UI
+	$('#player-stop').button({
+		icons: {primary: "ui-icon-stop"}
+	});
+	$('#player-play').button({
+		icons: {primary: "ui-icon-play"}
+	});
+	$('#player-pause').button({
+		icons: {primary: "ui-icon-pause"}
+	});
+
+	$('#player-open-folder').button({
+		text: false,
+		icons: {primary: "ui-icon-folder-collapsed"}
+	});
+	$('#player-open-file').button({
+		text: false,
+		icons: {primary: "ui-icon-arrowthickstop-1-s"}
+	});
+	
+	$("#player-progress").progressbar({"value":0.1, "max":100}).hover(
+		function() { $(this).find('.ui-progressbar-value').addClass('ui-state-hover'); },
+		function() { $(this).find('.ui-progressbar-value').removeClass('ui-state-hover') }
+	);
+});
