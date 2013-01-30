1734128fc7c8c63a5d8dd43d5945ee12fdcf6849
boukeversteegh <boukeversteegh@gmail.com>
2013-01-30 11:36:06 +0100

    Added remove button to playlist

--- www/js/playlistview.js
+++ www/js/playlistview.js
@@ -8,7 +8,10 @@ function PlaylistView(playlist, target) {
 	this.refresh = function() {
 		var self = this;
 		view.render('/www/tpl/playlist.html', {data: this.playlist}, this.target).then(function() {
-			$(self.target).find('.playlist-play').empty().button({text:false, icons: {primary: 'ui-icon-play'}});
+			var targetdomobject = $(self.target);
+			targetdomobject.find('.playlist-play').empty().button({text:false, icons: {primary: 'ui-icon-play'}});
+			targetdomobject.find('.playlist-remove').empty().button({text:false, icons: {primary: 'ui-icon-close'}});
+			
 			$('#files').find('.playlist-add').removeClass('ui-state-focus');
 			for( var i=0; i<self.playlist.items.length; i++) {
 				var item = self.playlist.items[i];
